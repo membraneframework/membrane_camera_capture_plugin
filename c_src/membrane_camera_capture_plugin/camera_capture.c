6b869719d79bf0709998f1468ce6c62cabeda4a5
Dima Mikielewicz <dymitrmikielewicz@gmail.com>
2022-03-26 12:03:36 +0100

    Fixed const

--- c_src/membrane_camera_capture_plugin/camera_capture.c
+++ c_src/membrane_camera_capture_plugin/camera_capture.c
@@ -16,7 +16,7 @@ UNIFEX_TERM do_open(UnifexEnv *env, char *url, char *framerate) {
   State *state = unifex_alloc_state(env);
   UNIFEX_TERM ret;
 
-  const AVInputFormat *input_format = av_find_input_format(driver);
+  AVInputFormat *input_format = av_find_input_format(driver);
   if (input_format == NULL) {
     ret = do_open_result_error(env, "Could not open input");
     goto end;
