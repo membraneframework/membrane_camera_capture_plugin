25018b6e0250745aa236ae9ec1fba1ed239e1d86
Billal GHILAS <bilal@evercam.io>
2023-01-31 21:56:36 +0100

    Read stream properties before sending stream format

--- c_src/membrane_camera_capture_plugin/camera_capture.spec.exs
+++ c_src/membrane_camera_capture_plugin/camera_capture.spec.exs
@@ -6,5 +6,6 @@ spec do_open(url :: string, framerate :: string) ::
        {:ok :: label, state} | {:error :: label, reason :: atom}
 
 spec read_packet(state) :: {:ok :: label, payload} | {:error :: label, reason :: atom}
+spec stream_props(state) :: {:ok :: label, width :: int, height :: int, pixel_format :: string}
 
 dirty :cpu, read_packet: 1
