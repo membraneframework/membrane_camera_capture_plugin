32e5c24a368f2fbc696a2572b065fc6d4e636381
Noarkhh <jakub.pryc@gmail.com>
2023-10-09 14:24:41 +0200

    Fix readme and bundlex version

--- mix.exs
+++ mix.exs
@@ -39,8 +39,7 @@ defmodule Membrane.CameraCapture.Mixfile do
   defp deps do
     [
       {:membrane_core, "~> 0.12.0"},
-      {:unifex, "~> 1.1.0"},
-      {:bundlex, "~> 1.2.0"},
+      {:bundlex, "~> 1.2"},
       {:membrane_raw_video_format, "~> 0.3.0"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
