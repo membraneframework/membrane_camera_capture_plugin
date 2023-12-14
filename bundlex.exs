d0060455572ac11e807601e8c7fd59dec0b55d13
Jakub Pryc <94321002+Noarkhh@users.noreply.github.com>
2023-12-14 11:17:31 +0100

    Start using Precompiled Dependency Provider (#22)
    * Start using Precompiled Dependency Provider

--- bundlex.exs
+++ bundlex.exs
@@ -1,26 +1,6 @@
 defmodule Membrane.CameraCapture.BundlexProject do
   use Bundlex.Project
 
-  defp get_ffmpeg_url() do
-    membrane_precompiled_url_prefix =
-      "https://github.com/membraneframework-precompiled/precompiled_ffmpeg/releases/latest/download/ffmpeg"
-
-    case Bundlex.get_target() do
-      %{os: "linux"} ->
-        {:precompiled,
-         "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n6.0-latest-linux64-gpl-shared-6.0.tar.xz"}
-
-      %{architecture: "x86_64", os: "darwin" <> _rest_of_os_name} ->
-        {:precompiled, "#{membrane_precompiled_url_prefix}_macos_intel.tar.gz"}
-
-      %{architecture: "aarch64", os: "darwin" <> _rest_of_os_name} ->
-        {:precompiled, "#{membrane_precompiled_url_prefix}_macos_arm.tar.gz"}
-
-      _other ->
-        nil
-    end
-  end
-
   def project do
     [
       natives: natives()
@@ -33,7 +13,11 @@ defmodule Membrane.CameraCapture.BundlexProject do
         interface: :nif,
         sources: ["camera_capture.c"],
         os_deps: [
-          {[get_ffmpeg_url(), :pkg_config], ["libavformat", "libavutil", "libavdevice"]}
+          ffmpeg: [
+            {:precompiled, Membrane.PrecompiledDependencyProvider.get_dependency_url(:ffmpeg),
+             ["libavformat", "libavutil", "libavdevice"]},
+            {:pkg_config, ["libavformat", "libavutil", "libavdevice"]}
+          ]
         ],
         preprocessor: Unifex
       ]
