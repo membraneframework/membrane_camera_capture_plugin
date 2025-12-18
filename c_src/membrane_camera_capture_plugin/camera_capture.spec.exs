f04a3ec3904cd1751c9a253d4128ce44c4fd7538
Feliks Pobiedziński <38541925+FelonEkonom@users.noreply.github.com>
2025-12-18 12:40:53 +0100

    Open camera on a dirty scheduler (#25)
    * Open camera on dirty scheduler
    
    * Apply reviewr suggestion

--- c_src/membrane_camera_capture_plugin/camera_capture.spec.exs
+++ c_src/membrane_camera_capture_plugin/camera_capture.spec.exs
@@ -8,4 +8,4 @@ spec do_open(url :: string, framerate :: string) ::
 spec read_packet(state) :: {:ok :: label, payload} | {:error :: label, reason :: atom}
 spec stream_props(state) :: {:ok :: label, width :: int, height :: int, pixel_format :: string}
 
-dirty :cpu, read_packet: 1
+dirty :io, read_packet: 1, do_open: 2
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.CameraCapture.Mixfile do
   use Mix.Project
 
-  @version "0.7.4"
+  @version "0.7.5"
   @github_url "https://github.com/membraneframework/membrane_camera_capture_plugin"
 
   def project do
