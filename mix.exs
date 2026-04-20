8abed21997cd1e0d7355be02f7c1a01966ad97c2
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2026-04-20 13:17:36 +0200

    Update mix.lock

--- mix.exs
+++ mix.exs
@@ -43,9 +43,9 @@ defmodule Membrane.CameraCapture.Mixfile do
       {:unifex, "~> 1.0"},
       {:membrane_precompiled_dependency_provider, "~> 0.2.1"},
       {:membrane_raw_video_format, "~> 0.4.3"},
-      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
-      {:credo, ">= 0.0.0", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.4", only: :dev, runtime: false},
+      {:credo, "~> 1.7", only: :dev, runtime: false},
       {:membrane_ffmpeg_swscale_plugin, "~> 0.16.0", only: :test},
       {:membrane_sdl_plugin, ">= 0.0.0", only: :test}
     ]
@@ -53,11 +53,13 @@ defmodule Membrane.CameraCapture.Mixfile do
 
   defp dialyzer() do
     opts = [
-      flags: [:error_handling]
+      flags: [:error_handling],
+      plt_add_apps: [:syntax_tools]
     ]
 
     if System.get_env("CI") == "true" do
       # Store PLTs in cacheable directory for CI
+      File.mkdir_p!(Path.join([__DIR__, "priv", "plts"]))
       [plt_local_path: "priv/plts", plt_core_path: "priv/plts"] ++ opts
     else
       opts
